27897e07510967f593731aa9b13170167e6cb732
Ren <rpaczkowski@gmail.com>
2022-08-10 21:49:58 -0400

    Began creating form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,3 +1,14 @@
 //search bar w three option of criteria to search by
 
+import { useState } from "react";
 import { MoviesList } from "./MovieList";
+
+function SearchForm() {
+  const [movieList, setMovieList] = useState<MoviesList[]>([]);
+
+  return (
+    
+  )
+}
+
+export default SearchForm;
