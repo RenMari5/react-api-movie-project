41ed75762d50e133be074a5c8c61fe590a5d9c54
auriellove <auriel.e.love@gmail.com>
2022-08-10 21:14:41 -0400

    movie interface

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,1 +1,10 @@
 // displays icons and description for each movie shown
+
+export interface MoviesList {
+  id: number;
+  originalTitle: string;
+  posterPath: string;
+  overview: string;
+  releaseDate: string;
+  popularity: number;
+}
