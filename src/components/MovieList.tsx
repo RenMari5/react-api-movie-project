c4257d707cb65fcd77675387dd32050044222709
auriellove <auriel.e.love@gmail.com>
2022-08-10 21:32:40 -0400

    types file

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,10 +1,1 @@
 // displays icons and description for each movie shown
-
-export interface MoviesList {
-  id: number;
-  title: string;
-  posterPath: string;
-  overview: string;
-  releaseDate: string;
-  popularity: number;
-}
---
+++ src/types/movies.ts
@@ -0,0 +1,8 @@
+export interface MoviesList {
+  id: number;
+  title: string;
+  posterPath: string;
+  overview: string;
+  releaseDate: string;
+  popularity: number;
+}
