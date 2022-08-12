460ee9bf24d6f02adf3f9160b5c3c8e201322b98
Ren <rpaczkowski@gmail.com>
2022-08-11 21:40:20 -0400

    Working on adding search filters

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,14 +1,33 @@
 //search bar w three option of criteria to search by
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { json } from "stream/consumers";
 import { Movie } from "../types/movies";
 
 function SearchForm() {
-  const [movieList, setMovieList] = useState<Movie[]>([]);
+  const [movieList, setMovieList] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=a756cffa90dee1edf6db15c8b94e2973&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setMovieList(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-
     clearSearchValues();
   }
 
