d53ce88de96b1e934c3f65b9ffba461eceaa7e20
auriellove <auriel.e.love@gmail.com>
2022-08-15 19:20:09 -0400

    changed type and worked on movielist

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,1 +1,13 @@
 // displays icons and description for each movie shown
+import { MovieContext } from "../context/MovieContext";
+import { MovieContextProvider } from "../context/MovieContextProvider";
+import { Movie } from "../types/movies";
+import { movieListing } from "./MovieListItem";
+
+export default function MovieOptions() {
+  return (
+    <div>
+      <ul>{/*  */}</ul>
+    </div>
+  );
+}
--- src/types/movies.ts
+++ src/types/movies.ts
@@ -1,10 +1,10 @@
 export type Movie = {
-  id?: number;
+  id: number;
   title: string;
-  posterPath?: string;
-  overview?: string;
-  releaseDate?: string;
-  popularity?: number;
+  posterPath: string;
+  overview: string;
+  releaseDate: string;
+  popularity: number;
   isOnWatchList?: boolean;
   isFavorited?: boolean;
-}
+};
