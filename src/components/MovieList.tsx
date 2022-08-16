8c67c1ab2312c255f5ca5c127b8232281e36f3ec
auriellove <auriel.e.love@gmail.com>
2022-08-15 20:54:16 -0400

    useeffect

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,13 +1,18 @@
 // displays icons and description for each movie shown
+import { useEffect, useState } from "react";
+import fetchMovies from "../services/movie.service";
 import { MovieContext } from "../context/MovieContext";
 import { MovieContextProvider } from "../context/MovieContextProvider";
 import { Movie } from "../types/movies";
-import { movieListing } from "./MovieListItem";
 
 export default function MovieOptions() {
-  return (
-    <div>
-      <ul>{/*  */}</ul>
-    </div>
-  );
+  const [movies, setMovies] = useState<Movie[]>([]);
+  useEffect(() => {
+    fetchMovies().then((movies) => {
+      console.log(movies);
+      setMovies(movies);
+    });
+  }, []);
+
+  return <div>//map--where we are fetching all the data</div>;
 }
