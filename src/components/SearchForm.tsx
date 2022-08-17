19fc5ea8208717fa30bfec851ad3d3df264efc4c
Ren <rpaczkowski@gmail.com>
2022-08-17 19:57:25 -0400

    added tailwind and a little more to searchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -80,12 +80,25 @@ function SearchForm() {
               aria-label="Filter Movies by Genre"
             >
               <option value="All">Filter by Genre</option>
-              <option value="Romantic Comedy">Romantic Comedy</option>
-              <option value="Horror">Horror</option>
-              <option value="Comedy">Comedy</option>
               <option value="Action">Action</option>
+              <option value="Adventure">Adventure</option>
+              <option value="Animation">Animation</option>
+              <option value="Comedy">Comedy</option>
+              <option value="Crime">Crime</option>
+              <option value="Documentary">Documentary</option>
               <option value="Drama">Drama</option>
-              <option value="Indie">Indie</option>
+              <option value="Family">Family</option>
+              <option value="Fantasy">Fantasy</option>
+              <option value="History">History</option>
+              <option value="Horror">Horror</option>
+              <option value="Music">Music</option>
+              <option value="Mystery">Mystery</option>
+              <option value="Romance">Romance</option>
+              <option value="Science Fiction">Science Fiction</option>
+              <option value="TV Movie">TV Movie</option>
+              <option value="Thriller">Thriller</option>
+              <option value="War">War</option>
+              <option value="Western">Western</option>
             </select>
           </label>
           <input type="submit">Submit</input>
