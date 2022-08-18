f2f4e5aff227c96e691dfce0e29450a0d6e35185
Ken Cargill <ken.0.cargill@gmail.com>
2022-08-17 22:09:55 -0400

    fixing how we rec movie detail info

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,44 +1,42 @@
 import { Movie } from "../types/movies";
 import { useContext, useState, useEffect } from "react";
 import { MovieContext } from "../context/MovieContext";
-import fetchMovies from "../services/movie.service";
+import { fetchMovie } from "../services/movie.service";
+import { useParams } from "react-router-dom";
  
-interface RouteParams {
-    id: string;
-}
 
 export default function MovieDetail () {
     // onClick on poster in movie list to bring it to this page by id
     // <Link to={`/details/${item.id}`}>details</Link> on movie items to link to details page
-    const { id } = useParams<RouteParams>();
+    const { id } = useParams();
     const { moviesToWatch, moviesFavorited, addMovieToFavList, addMovieToWatchList, removeMovieFromFavList, removeMovieFromWatchList } = useContext(MovieContext); //do we need to use MovieContextProvider?
     const [movies, setMovies] = useState<Movie[]>([]);
-    const movie = movies.find((movie) => movie.id === id); // why is this acting like the type includes undefined?
+    const movie = movies.find((movie) => movie.id === Number(id));
 
     useEffect(() => {
-      fetchMovies().then((movies) => {
+      fetchMovie(id as string).then((movies) => {
         console.log(movies);
         setMovies(movies);
       });
-    }, []);
+    }, [id]);
     
     return (
-    <div className="MovieRoute">
-        <p>{movie?.title}</p>
-        <img src={`${movie?.poster_path}`} alt='movie poster' />
-        <p>{movie?.overview}</p>
-        <p>Released: {movie?.release_date}</p>
-        <p>Popularity: ${movie?.popularity}</p>
-        {moviesFavorited.find((movie) => movie.id === id) ? 
-            <button onClick={() => addMovieToFavList(movie)}>favorited</button> :
-            <button onClick={() => removeMovieFromFavList(id)}>favorited</button>}
-        {moviesToWatch.find((movie) => movie.id === id)  ? 
-            <button onClick={() => addMovieToWatchList(movie)}>favorited</button> :
-            <button onClick={() => removeMovieFromWatchList(id)}>favorited</button>} 
-            </div>
-    )
-
-    function useParams<T>(): { id: any; } {
-        throw new Error("Function not implemented.");
-    }
-}
+        <div>
+            {/* { movie !== undefined?
+            //     <div className="MovieRoute">
+            //         <p>{movie.title}</p>
+            //         <img src={`${movie.poster_path}`} alt='movie poster' />
+            //         <p>{movie.overview}</p>
+            //         <p>Released: {movie.release_date}</p>
+            //         <p>Popularity: ${movie.popularity}</p>
+            //         {moviesFavorited.find((movie) => movie.id === Number(id)) ? 
+            //             <button onClick={() => addMovieToFavList(movie)}>favorited</button> :
+            //             <button onClick={() => removeMovieFromFavList(Number(id))}>favorited</button>}
+            //         {moviesToWatch.find((movie) => movie.id === Number(id))  ? 
+            //             <button onClick={() => addMovieToWatchList(movie)}>favorited</button> :
+            //             <button onClick={() => removeMovieFromWatchList(Number(id))}>favorited</button>} 
+            //     </div> :
+            // <p>oops! please retry</p>
+            } */}
+        </div>
+    )}
--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,10 +1,19 @@
 import axios from "axios";
+import { config } from "../config";
 import { Movie, MovieResults } from "../types/movies";
 
-export default function fetchMovies() {
+export function fetchMovies() {
   return axios
     .get<MovieResults>(
       `https://api.themoviedb.org/3/discover/movie?api_key=${config.apiKey}`
     )
     .then((response) => response.data.results);
+}
+
+export function fetchMovie (id: string) {
+  return axios
+  .get<MovieResults>(
+    `https://api.themoviedb.org/3/${id}?api_key=${config.apiKey}&language=en-US&external_source=imdb_id`
+  )
+  .then((response) => response.data.results);
 }
