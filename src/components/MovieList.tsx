7469ef43d8796159f464264e69b8225c40efbdad
Ken Cargill <ken.0.cargill@gmail.com>
2022-08-18 18:38:54 -0400

    fixed errors

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,11 +1,9 @@
-// displays icons and description for each movie shown
 import { useEffect, useState } from "react";
-import fetchMovies from "../services/movie.service";
-import { MovieContext } from "../context/MovieContext";
-import { MovieContextProvider } from "../context/MovieContextProvider";
+import { fetchMovies } from "../services/movie.service";
+// import { MovieContext } from "../context/MovieContext";
+// import { MovieContextProvider } from "../context/MovieContextProvider";
 import { Movie } from "../types/movies";
 import { MovieListItem } from "./MovieListItem";
-// import MovieListItem from "./MovieListItem";
 
 export default function MovieOptions() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -18,7 +16,6 @@ export default function MovieOptions() {
 
   return (
     <div>
-      //map--where we are fetching all the data
       {movies.map((item, index) => (
         <MovieListItem item={item} key={index} />
       ))}
