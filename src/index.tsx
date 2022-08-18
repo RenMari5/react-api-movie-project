bdf6ecaf9917393492ec59c1027960325173bad1
Ken Cargill <ken.0.cargill@gmail.com>
2022-08-18 18:44:54 -0400

    added BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { MovieContextProvider } from './context/MovieContextProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MovieContextProvider>
-      <App />
-    </MovieContextProvider>
+    <BrowserRouter>
+      <MovieContextProvider>
+        <App />
+      </MovieContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
