d55e59c0b729ee45744cb14d886a3de718cb4f68
Ren <rpaczkowski@gmail.com>
2022-08-24 09:10:25 -0400

    Changed alt text for posters to read the movie title

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -3,44 +3,69 @@ import { useContext, useState, useEffect } from "react";
 import { MovieContext } from "../context/MovieContext";
 import { fetchMovie } from "../services/movie.service";
 import { useParams } from "react-router-dom";
- 
+
 interface MovieDetailProps {
   children?: React.ReactNode;
 }
 
-export default function MovieDetail ( { children }: MovieDetailProps ) {
-    // onClick on poster in movie list to bring it to this page by id
-    // <Link to={`/details/${item.id}`}>details</Link> on movie items to link to details page
-    const { id } = useParams();
-    const { moviesToWatch, moviesFavorited, addMovieToFavList, addMovieToWatchList, removeMovieFromFavList, removeMovieFromWatchList } = useContext(MovieContext); //do we need to use MovieContextProvider?
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const movie = movies.find((movie) => movie.id === Number(id));
+export default function MovieDetail({ children }: MovieDetailProps) {
+  // onClick on poster in movie list to bring it to this page by id
+  // <Link to={`/details/${item.id}`}>details</Link> on movie items to link to details page
+  const { id } = useParams();
+  const {
+    moviesToWatch,
+    moviesFavorited,
+    addMovieToFavList,
+    addMovieToWatchList,
+    removeMovieFromFavList,
+    removeMovieFromWatchList,
+  } = useContext(MovieContext); //do we need to use MovieContextProvider?
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const movie = movies.find((movie) => movie.id === Number(id));
 
-    useEffect(() => {
-      fetchMovie(id as string).then((movies) => {
-        console.log(movies);
-        setMovies(movies);
-      });
-    }, [id]);
-    
-    return (
-        <div>
-          { children }
-            { movie !== undefined ?
-                <div className="MovieRoute">
-                    <p>{movie.title}</p>
-                    <img src={`${movie.poster_path}`} alt='movie poster' />
-                    <p>{movie.overview}</p>
-                    <p>Released: {movie.release_date}</p>
-                    <p>Popularity: ${movie.popularity}</p>
-                    {moviesFavorited.find((movie) => movie.id === Number(id)) ? 
-                        <button onClick={() => addMovieToFavList(movie)} value="add to favorites" /> :
-                        <button onClick={() => removeMovieFromFavList(Number(id))} value="remove from favorites" />}
-                    {moviesToWatch.find((movie) => movie.id === Number(id))  ? 
-                        <button onClick={() => addMovieToWatchList(movie)} value= "add to watch list" /> :
-                        <button onClick={() => removeMovieFromWatchList(Number(id))} value="remove from watch list" />} 
-                </div> :
-            <p>oops! please retry</p>
-            }
+  useEffect(() => {
+    fetchMovie(id as string).then((movies) => {
+      console.log(movies);
+      setMovies(movies);
+    });
+  }, [id]);
+
+  return (
+    <div>
+      {children}
+      {movie !== undefined ? (
+        <div className="MovieRoute">
+          <p>{movie.title}</p>
+          <img src={movie.poster_path} alt={`${movie.title} poster`} />
+          <p>{movie.overview}</p>
+          <p>Released: {movie.release_date}</p>
+          <p>Popularity: ${movie.popularity}</p>
+          {moviesFavorited.find((movie) => movie.id === Number(id)) ? (
+            <button
+              onClick={() => addMovieToFavList(movie)}
+              value="add to favorites"
+            />
+          ) : (
+            <button
+              onClick={() => removeMovieFromFavList(Number(id))}
+              value="remove from favorites"
+            />
+          )}
+          {moviesToWatch.find((movie) => movie.id === Number(id)) ? (
+            <button
+              onClick={() => addMovieToWatchList(movie)}
+              value="add to watch list"
+            />
+          ) : (
+            <button
+              onClick={() => removeMovieFromWatchList(Number(id))}
+              value="remove from watch list"
+            />
+          )}
         </div>
-    )}
+      ) : (
+        <p>oops! please retry</p>
+      )}
+    </div>
+  );
+}
